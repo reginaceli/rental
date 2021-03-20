a2d149e6be01c7966c75587b0152d86df8578dba
Regina Celi da Silva <regicelis7@hotmail.com>
2021-03-20 18:47:49 -0300

    config server and update files

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('reload automatizado')
+
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request, response)=>{
+    response.json({ api: "Home Rental Cars"})
+})
+
+app.listen(3335, ()=>console.log("server running"))
